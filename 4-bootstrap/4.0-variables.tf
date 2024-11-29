9586c6f09deeff2db92d79ede2a24137a00caf24
rafael.cardona.it@gmail.com <rafael.cardona.it@gmail.com>
2024-11-29 23:29:43 +0100

    done

--- 4-bootstrap/4.0-variables.tf
+++ 4-bootstrap/4.0-variables.tf
@@ -2,13 +2,20 @@ variable "secret_id" {}
 
 variable "secret_key" {}
 
-variable "coreos_image_id" {
-  description = "Image ID to use for installation of the registry"
-  type        = string
-}
-
 variable "instance_type" {
   description = "Instance type to provision bootstrap"
   type        = string
+  default     = 100
+}
+
+variable "data_disk_size" {
+  description = "Instance type to provision bootstrap"
+  type        = string
   default     = "S3.MEDIUM4"
+}
+
+variable "data_disk_type" {
+  description = "Instance type to provision bootstrap"
+  type        = string
+  default     = "CLOUD_PREMIUM"
 }
