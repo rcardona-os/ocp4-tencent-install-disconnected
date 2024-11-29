304666ec13ec55c3deba7b728076406bd27d2294
rafael.cardona.it@gmail.com <rafael.cardona.it@gmail.com>
2024-11-29 21:47:09 +0100

    done

--- 3-ocp-infra/3.1-data-variables.tf
+++ 3-ocp-infra/3.1-data-variables.tf
@@ -1,4 +1,4 @@
-data "terraform_remote_state" "cloud_infra_for_ocp" {
+data "terraform_remote_state" "cloud_infra" {
   backend = "local"
   config = {
     path = "../0-cloud-infra/terraform.tfstate" # Relative path to project-a's state file
--- 3-ocp-infra/3.7-outputs.tf
+++ 3-ocp-infra/3.7-outputs.tf
@@ -1,12 +1,11 @@
-# Output the public IP address of the CVM
-output "registry_public_ip" {
-  value = tencentcloud_instance.registry.public_ip
+
+output "master_security_group_id" {
+  value = tencentcloud_security_group.master_security_group.id
 }
 
-output "ssh_key_pair_id" {
-  value = tencentcloud_key_pair.ssh_key_pair.id
+output "worker_security_group_id" {
+  value = tencentcloud_security_group.worker_security_group.id
 }
-
 
 output "ocp_private_subnet_1_id" {
   value = tencentcloud_subnet.ocp_private_subnet_1.id
