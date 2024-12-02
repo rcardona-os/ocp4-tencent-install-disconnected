d9a243653e76d1b1bfb49276742d598489cdcf6f
rafael.cardona.it@gmail.com <rafael.cardona.it@gmail.com>
2024-12-02 22:36:47 +0100

    done

--- 3-ocp-infra/3.6-ocp-load-balancer.tf
+++ 3-ocp-infra/3.6-ocp-load-balancer.tf
@@ -6,7 +6,8 @@ resource "tencentcloud_clb_instance" "master_lb" {
   clb_name     = "master-lb"
   vpc_id       = data.terraform_remote_state.cloud_infra.outputs.vpc_id       
   network_type = "INTERNAL"
-  vip          = tencentcloud_private_dns_record.api_server_a_record.record_value                              
+  vip          = tencentcloud_private_dns_record.api_server_a_record.record_value
+  subnet_id    = tencentcloud_subnet.ocp_private_subnet_1.id                             
 }
 
 resource "tencentcloud_clb_listener" "master_listener_6443" {
@@ -31,7 +32,8 @@ resource "tencentcloud_clb_instance" "worker_lb" {
   clb_name     = "worker-lb"
   vpc_id       = data.terraform_remote_state.cloud_infra.outputs.vpc_id       
   network_type = "INTERNAL"
-  vip          = tencentcloud_private_dns_record.app_ingress_a_record.record_value                                                         
+  vip          = tencentcloud_private_dns_record.app_ingress_a_record.record_value
+  subnet_id    = tencentcloud_subnet.ocp_private_subnet_1.id                                                        
 }
 
 resource "tencentcloud_clb_listener" "worker_listener_80" {
